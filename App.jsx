2d3fca531fe27d28415dfaad4545805a9ea08bb6
Vasundhara vats <vatsv2491@gmail.com>
2025-09-15 19:53:35 +0530

    login ui

---
+++ App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-red-500">
+      <h1 className="text-4xl font-bold text-white">
+        Tailwind is working 🎉
+      </h1>
+    </div>
+  );
+}
+
+export default App;
